84064c27a3b5d4cd4643a839127837ef3217ffa0
Arekhimself <alexis.fouques@outlook.fr>
2025-07-25 23:15:53 +0200

    1.0.11

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ let dev = process.env.NODE_ENV === 'dev';
 if (dev) {
     let appPath = path.resolve('./AppData/Launcher').replace(/\\/g, '/');
     if (!fs.existsSync(appPath)) fs.mkdirSync(appPath, { recursive: true });
-    app.setPath('userData', appPath);
+    app.setPath('userData', appPath); 
 }
 
 if (!app.requestSingleInstanceLock()) app.quit();
